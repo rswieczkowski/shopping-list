2c5f3d59cb4cedb323a0a743830c7e03930febc5
Rafał Świeczkowski <rswieczkowski@leonsoftware.com>
2023-04-15 11:56:39 +0200

    Clear UI state first second commit

--- script.js
+++ script.js
@@ -41,7 +41,10 @@ function createIcon(classes) {
 
 function removeItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
-    e.target.parentElement.parentElement.remove();
+    if (confirm('Are you sure?')) {
+      e.target.parentElement.parentElement.remove();
+      checkUI();
+    }
   }
 }
 
@@ -49,6 +52,7 @@ function clearItems(e) {
   while (itemList.firstChild) {
     itemList.removeChild(itemList.firstChild);
   }
+  checkUI();
 }
 
 function checkUI() {
